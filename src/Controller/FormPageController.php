a0341952809bce179ce565dbc52cd3dd53b237e8
Petr Popov <air-dev@yandex.ru>
2022-03-12 21:07:01 +0400

    Better error messages

--- src/Controller/FormPageController.php
+++ src/Controller/FormPageController.php
@@ -5,37 +5,35 @@ namespace App\Controller;
 use App\Form\InvoiceType;
 use App\Entity\Invoice;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class FormPageController extends AbstractController
 {
     public function index(Request $request, ManagerRegistry $doctrine): Response
     {
-        // just set up a fresh $task object (remove the example data)
         $invoice = new Invoice();
 
         $form = $this->createForm(InvoiceType::class, $invoice);
 
         $form->handleRequest($request);
-        $errors = [];
 
-        if ($form->isSubmitted()) {
-            if ($form->isValid()) {
-                $invoice = $form->getData();
+        if ($form->isSubmitted() && $form->isValid()) {
+            $invoice = $form->getData();
 
-                $entityManager = $doctrine->getManager();
-                $entityManager->persist($invoice);
-                $entityManager->flush();
-            } else {
-                $errors = $form->getErrors(true);
-            }
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($invoice);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('show');
         }
 
+
         return $this->renderForm('form-page.html.twig', [
-            'form' => $this->createForm(InvoiceType::class),
-            'errors' => $errors
+            'form' => $form
         ]);
     }
 }
